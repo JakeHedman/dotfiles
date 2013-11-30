e7b816dc64c2b58cd97a264fd7eb20f3ae06f0f6
Jakob Hedman <jakob.hedman+github@gmail.com>
2013-11-30 12:32:14 +0100

    Rounding is hard

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -11,9 +11,10 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local lain      = require("lain")
 
-function round(val, decimal)
-	local exp = decimal and 10^decimal or 1
-	return math.ceil(val * exp - 0.5) / exp
+function round(num, idp)
+	local mult = 10^(idp or 0)
+	if num >= 0 then return math.floor(num * mult + 0.5) / mult
+	else return math.ceil(num * mult - 0.5) / mult end
 end
 
 -- {{{ Error handling
