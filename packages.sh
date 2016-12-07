c326a4572e26dbd4747ef08220a442fe76d5240f
Jake Hedman <jake@proceranetworks.com>
2016-12-07 14:19:35 +0100

    Add script to install arch packages

---
+++ packages.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+sudo pacman --needed -Sy python xf86-video-intel mesa-libgl xorg i3 vulkan-intel lib32-mesa-libgl git curl sudo openssh dmenu zsh urxvt-perls jshon python2 ruby lua perl tcl
+sudo pacman --needed -Sy gvim
+
+# get aura
+which aura || curl https://raw.githubusercontent.com/keenerd/packer/master/packer | bash -s -- -S --noconfirm aura-bin
+
+# get nvm
+which nvm || curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+
+. ~/.nvm/nvm.sh
+nvm install stable
+nvm use stable
+
+sudo aura --needed -Ay spotify google-chrome rxvt-unicode-patched tldr-cpp-client
+
+if [ -f ~/.notebook ]; then
+    sudo pacman --needed -Sy connman
+    sudo aura --needed -Ay connman-ncurses
+fi
